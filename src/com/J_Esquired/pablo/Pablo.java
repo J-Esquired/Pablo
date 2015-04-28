9329b1ac6de65f5d4c62a3c097d68cd93614ea9e
Bee Hive Williams <period1@ArtMac2.local>
2015-04-28 10:46:15 -0700

    tried to display things

--- src/com/J_Esquired/pablo/Pablo.java
+++ src/com/J_Esquired/pablo/Pablo.java
@@ -1,11 +1,18 @@
 package com.J_Esquired.pablo;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
-public class Pablo extends JPanel
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Pablo
 {
+   static int width, height;
    static JFrame pabloFrame;
+   static Graphics2D graphics;
    public Pablo()
    {
       super();
@@ -15,7 +22,10 @@ public class Pablo extends JPanel
    {
       pabloFrame = new JFrame("beans");
       pabloFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      pabloFrame.setSize(1280, 720);
+      width = 1280;
+      height = 720;
+      pabloFrame.setSize(width, height);
+      pabloFrame.setLayout(null);
       pabloFrame.setVisible(true);
       try
       {
@@ -26,5 +36,18 @@ public class Pablo extends JPanel
          // TODO Auto-generated catch block
          e.printStackTrace();
       }
+      BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+      graphics = image.createGraphics();
+      graphics.setColor(new Color(0xff0000));
+      graphics.drawOval(50,70,70,50);
+      graphics.draw(new java.awt.Rectangle(width, height));
+      System.out.println("lol what");
+      pabloFrame.getContentPane().add(new JLabel(new ImageIcon(image)));
+//      JLabel wat = new JLabel(new ImageIcon("map_mockup.png"));
+      JLabel wat = new JLabel("oh man");
+      pabloFrame.getContentPane().add(wat);
+      wat.setLocation(50,50);
+      wat.setVisible(true);
+//      pabloFrame.getContentPane().setBackground(new Color(0x00ff00));
    }
 }
